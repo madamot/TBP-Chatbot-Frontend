1bbe792174d19724ba390309c5c44f2cf45c246a
Madamot <adam@madamot.com>
2020-08-18 22:15:52 +0100

    chatbot story setup

---
+++ src/components/Chatbot/Chatbot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { action } from '@storybook/addon-actions';
+
+
+export default function Chatbot() {
+
+    return (
+      <div className="list-items">
+        <p>Chatbot</p>
+      </div>
+    );
+}
