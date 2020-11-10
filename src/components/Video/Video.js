7ec8347a4da10fad2802e666c1b8f8fdf376381a
Madamot <adam@madamot.com>
2020-11-10 10:04:20 +0000

    remove video css file

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { action } from '@storybook/addon-actions';
 
-import './Video.css';
 
 import ReactPlayer from 'react-player';
 
