ac916a340825f97aac5d86d96a11a7e38067257e
Madamot <adam@madamot.com>
2020-11-13 14:59:04 +0000

    chatbot fetches history and displays test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import App from '../App';
-
-describe('Chatbot', () => {
-  test('renders existing messages', async () => {
-    const { queryByText, findAllByText } = render(<App />)
-
-    expect(queryByText(/hello world/)).not.toBeInTheDocument()
-
-    const helloWorld = findAllByText('hello world')
-
-    expect(await helloWorld).toHaveLength(2)
-  })
-})
-
 
 describe('App', () => {
   test('renders chatbot component', () => {
